1b55a004c9148500834d2a53438db2958ebf6ab7
Valeriy <valeriy.gnybidenko@gmail.com>
2022-04-13 18:44:29 +0300

    home and home-2 sections without fix done

---
+++ components/AppButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="btn">
+    <span class="btn__bg"></span>
+    <span class="btn__text">{{ text }}</span>
+    <span class="btn__arrow">
+      <svg
+        class="btn__svg-arrow"
+        width="23"
+        height="20"
+        viewBox="0 0 23 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M13.17 0L10.89 2.25L16.5 7.89H0V11.28H16.47L10.89 16.89L13.17 19.14L22.2 9.75V9.39L13.17 0Z"
+          fill="#333333"
+        />
+      </svg>
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <main class="landing">
-    <div class="container landing__wrapper">Hello</div>
+  <main>
+    <section class="section home">
+      <div class="home__bg"></div>
+      <div class="container home__wrapper">
+        <div class="home__block">
+          <h1 class="home__title">Your Assets, Your Dashboard.</h1>
+          <p class="home__desc">
+            A crypto portfolio tracker designed for a new generation of digital
+            assets.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="section home-2">
+      <div class="container grid home-2__wrapper">
+        <img class="home-2__img" src="/images/home-2.jpg" alt="Image" />
+        <div class="home-2__block">
+          <h2 class="home-2__title">Everything In One Place</h2>
+          <p class="home-2__desc">
+            Track the performance of your crypto assets across a wide variety of
+            popular exchanges, blockchain networks and DeFi protocols with just
+            one app.
+          </p>
+          <app-button class="home-2__btn" text="Quick Start" />
+        </div>
+      </div>
+    </section>
+    <section class="section home-3">
+      <div class="container grid home-3__wrapper"></div>
+    </section>
   </main>
 </template>
+
+<script>
+import AppButton from '~/components/AppButton.vue'
+
+export default {
+  components: { AppButton },
+}
+</script>
