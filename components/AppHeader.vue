c17eaecbb6d1e6b97ab044b699e9addb956c4e34
Valeriy <valeriy.gnybidenko@gmail.com>
2022-04-13 11:18:31 +0300

    add fonts

---
+++ components/AppHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="header navbar">
+    <nav class="container header__nav"></nav>
+  </header>
+</template>
--- gulp-src/fonts-include.js
+++ gulp-src/fonts-include.js
@@ -1,1 +1,49 @@
-export const fi = []
+export const fi = [
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/Moderat-Bold.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/Moderat-Medium.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/Moderat-Mono-Bold.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/Moderat-Mono-Medium.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/Moderat-Mono-Regular.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/Moderat-Regular.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+]
