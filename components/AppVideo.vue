138b992cd9efaffb9c0d1cd54ad114a8ee1b7fe2
letmewin22 <lkosteckiy5@gmail.com>
2022-04-15 16:05:11 +0300

    update videos

--- components/AppVideo.vue
+++ components/AppVideo.vue
@@ -1,6 +1,6 @@
 <template>
   <video playsinline="" muted autoplay loop>
-    <source :src="`${link}.webm`" type="video/webm" />
+    <!-- <source :src="`${link}.webm`" type="video/webm" /> -->
     <source :src="`${link}.mp4`" type="video/mp4" />
   </video>
 </template>
